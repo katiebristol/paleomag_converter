e7de4d99a90e646257f6e3fd8f33c70441e791dc
Katie Bristol <kebristo@mtu.edu>
2022-07-03 15:13:42 -0400

    Initializing repo files/folders

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # paleomag_converter
 A basic Python script to convert individual sample files previously made by Data Splitter into generic PmagPy format for IZZI. Output files are compatible with PmagPy or paleomagnetism.org. 
+# PaleoMag Converter
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](LICENSE)
+
+## Purpose
+
+This script was created to convert individual sample files previously
+made by Data Splitter into generic PmagPy format. Files produced by this 
+script can be used with PmagPy and paleomagnetism.org. Field intensity
+is output in A/m assuming a sample volume of 10cc. 
+
+![Demo data.](/assets/input-output.png)
+
+## How to use
+
+1) Put individual sample files (such as those split by [Data Splitter](https://github.com/katiebristol/data_splitter)) that you want 
+converted into the /input/ folder.
+2) Run the script
+3) Check the /output/ folder for your converted files. 
+
+## Inputs
+
+This script requires .dat files that you want converted placed into the
+/input/ folder. 
+
+## Outputs
+
+This script will create converted files with the same name. 
+The converted files will write to the /output/ folder. 
+
+## Notes
+
+This script was made in conjunction with the Data Splitter script.
+It is intended to be used afterwards to convert data to PmagPy generic/paleomagnetism.org friendly formats.
+
+## License
+
+This script is published under the [MIT license](LICENSE.txt).
---
+++ src/paleomag_converter.py
@@ -0,0 +1,40 @@
+import os.path
+from os import path
+from pathlib import Path
+
+headers = ["specimen", "treatment", "treatment_type", "moment", "dec_s", "inc_s", "dec_g", "inc_g"]
+headers = " ".join(headers)
+rootPath = Path('./input')
+inputList = [e for e in os.listdir(rootPath) if not os.path.isdir(e)]
+for currentInput in inputList:
+    line_number = 0
+    header = ''
+    add_header = False
+    with open('./input/' + currentInput, "r") as a_file:
+        write_file = open('output/' + currentInput, 'a+')
+        write_file.write(str(headers.replace(' ', '\t'))+'\n')
+        for line in a_file:
+            line_number+=1
+            next_line = " ".join(line.strip().split()) #Remove duplicate spaces
+            next_line = next_line.replace(" ", ",") #Convert spaces to commas
+
+            next_line_array = next_line.split(",")
+            
+            
+            if line_number == 1: #Ignore header line (line 1)
+                header = header + next_line + '\n' #Unused addition to header variable
+                continue
+            try:
+                #Edit array
+                next_line_array = next_line_array[0:len(headers)] 
+                del next_line_array[3]
+                print(next_line_array)
+                next_line_array[3] = float(next_line_array[3])*10**-2 #Convert from Oe to A/m (Cryo assumes 10cc vol.)
+                #stop editing the array
+                
+                next_line = " ".join(str(x) for x in next_line_array).replace("Cm","T") #Array to string, replacing Cm with T
+                write_file.write(str(next_line.replace(' ', '\t'))+'\n') #generic_paleomag.txt is like a csv but tabs instead of commas replace space with tab
+            except IndexError:
+                print("Index out of range error on this:")
+                print(next_line_array)
+print("Conversion finished. Check the /output/ folder for your converted files. ")
